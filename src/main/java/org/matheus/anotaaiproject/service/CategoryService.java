0ecca585274cb26da280a316eb363de1ccbe4acf
Mathe <matheusamrl@gmail.com>
2024-05-22 16:19:03 -0300

    Category now can publish at aws SNS

--- src/main/java/org/matheus/anotaaiproject/service/CategoryService.java
+++ src/main/java/org/matheus/anotaaiproject/service/CategoryService.java
@@ -1,8 +1,11 @@
 package org.matheus.anotaaiproject.service;
 
+import lombok.RequiredArgsConstructor;
 import org.matheus.anotaaiproject.entities.Category;
 import org.matheus.anotaaiproject.entities.DTOs.CategoryDTO;
 import org.matheus.anotaaiproject.repositories.CategoryRepository;
+import org.matheus.anotaaiproject.service.Aws.AwsSnsService;
+import org.matheus.anotaaiproject.service.Aws.MessageDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,13 +14,16 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
 
-    @Autowired
-    CategoryRepository categoryRepository;
+
+    private final CategoryRepository categoryRepository;
+    private final AwsSnsService awsSnsService;
 
     public Category create(CategoryDTO categoryData) {
         Category category = new Category(categoryData);
+        awsSnsService.publish(new MessageDTO(category.toString()));
         return categoryRepository.save(category);
     }
 
@@ -30,6 +36,7 @@ public class CategoryService {
         if(categoryFound.isPresent()){
             Category category1 = new Category(category);
             category1.setId(id);
+            awsSnsService.publish(new MessageDTO(category1.toString()));
             categoryRepository.save(category1);
             return ResponseEntity.ok(category1);
         }
